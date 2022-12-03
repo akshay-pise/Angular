700a434f5eac789b9a6193df1638825876bbdca6
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2022-12-03 22:16:10 +0530

    import form Module

--- applicationForm/src/app/app.module.ts
+++ applicationForm/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalFormComponent } from './component/personal-form/personal-form.component';
@@ -12,7 +12,8 @@ import { PersonalFormComponent } from './component/personal-form/personal-form.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- applicationForm/src/app/component/personal-form/personal-form.component.ts
+++ applicationForm/src/app/component/personal-form/personal-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-form',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalFormComponent implements OnInit {
 
-  constructor() { }
+  regform:FormGroup;
+  constructor() {
+    this.regform=new FormGroup({
+      firstname:new FormControl(''),
+      lastname:new FormControl(''),
+      bdate:new FormControl(''),
+      // gender:new FormControl(''),
+      email:new FormControl(''),
+      phoneno:new FormControl('')
+    })
+   }
 
   ngOnInit(): void {
   }
