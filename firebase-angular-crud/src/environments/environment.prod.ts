c81111d857602e893c1c4c90816cab3bf5eeb17f
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2022-12-13 14:30:05 +0530

    firebase setupcompleted

--- firebase-angular-crud/src/environments/environment.prod.ts
+++ firebase-angular-crud/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBR28F1ZOmzwGCpnb35h8FkruP1pcyfIFw",
+    authDomain: "fir-angular-crud-a9ff9.firebaseapp.com",
+    projectId: "fir-angular-crud-a9ff9",
+    storageBucket: "fir-angular-crud-a9ff9.appspot.com",
+    messagingSenderId: "604422688848",
+    appId: "1:604422688848:web:fb131e46b3e1b07a920155"
+  }
 };
