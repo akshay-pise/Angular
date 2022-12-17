8412478393090b2c682bc6a43a3aac84234a5363
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2022-12-17 13:19:04 +0530

    object of object print.

--- objectOfApi/src/app/app.component.ts
+++ objectOfApi/src/app/app.component.ts
@@ -7,35 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  demoList :any[]=[];
-  demo:any;
-
-  constructor(private http : HttpClient ) { }
-
+  demoList: any[] = [];
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
-    this.getAllData();
+      this.http.get<any[]>("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo")
+      .subscribe((data: any) => {
+        const array = Object.entries(data["Time Series (5min)"]).map((e) => ({
+          [e[0]]: e[1]
+        }
+        ));
+        this.demoList = array;
+      });
   }
-  getAllData()
-  {
-    debugger;
-    this.http.get<any[]>("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo")
-  .subscribe((data:any) => {
-
-    const array=Object.entries(data["Time Series (5min)"]).map((e)=>({
-      [e[0]]:e[1]}
-    ));
-    this.demoList=array;
-    let newArr = this.demoList.map((item) => {
-      return  {
-        open:item['1. open'],
-        name: item['2. high']
-      }
-
-  });
-      console.log('newarr',newArr);
-    // this.demoList=data['Time Series (5min)'];
-    console.log(JSON.stringify(this.demoList));
-  })
-}
+  getkey(item: any) {
+    return Object.keys(item)[0]
+  }
 }
