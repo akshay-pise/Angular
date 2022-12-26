f46560692d46d1245711d23dba93db2901cacfb4
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2022-12-26 23:29:28 +0530

    three module added in ott project

--- oTT/src/app/app-routing.module.ts
+++ oTT/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'films',
+    loadChildren: () => import('./pages/film/film.module').then(m => m.FilmModule)
+  },
+  {
+    path: 'media',
+    loadChildren: () => import('./pages/media/media.module').then(m => m.MediaModule)
+  },
+  { path: 'music',
+   loadChildren: () => import('./pages/music/music.module').then(m => m.MusicModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
