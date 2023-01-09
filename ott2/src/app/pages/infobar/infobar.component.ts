50b4f62b9b3aee5558a06b2158f29a10a3322acb
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2023-01-09 09:26:35 +0530

    infobar con added

--- ott2/src/app/pages/infobar/infobar.component.ts
+++ ott2/src/app/pages/infobar/infobar.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfobarComponent implements OnInit {
 
+  droplist: boolean = false;
+  droplist1: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  changeicon() {
+    (this.droplist === true)?(this.droplist = false):(this.droplist = true);
+  }
+  changeicon1() {
+    (this.droplist1 === true)?(this.droplist1 = false):(this.droplist1 = true);
+  }
 }
