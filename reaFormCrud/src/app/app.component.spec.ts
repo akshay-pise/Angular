76c6546ba8fde4dfbd518a1a042d5eb34cf9043c
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2023-01-31 13:20:49 +0530

    firebase paroject removed

--- reaFormCrud/src/app/app.component.spec.ts
+++ reaFormCrud/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firebase-angular-crud'`, () => {
+  it(`should have as title 'reaFormCrud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firebase-angular-crud');
+    expect(app.title).toEqual('reaFormCrud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('firebase-angular-crud app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('reaFormCrud app is running!');
   });
 });
--- reaFormCrud/src/environments/environment.ts
+++ reaFormCrud/src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyBR28F1ZOmzwGCpnb35h8FkruP1pcyfIFw",
-    authDomain: "fir-angular-crud-a9ff9.firebaseapp.com",
-    projectId: "fir-angular-crud-a9ff9",
-    storageBucket: "fir-angular-crud-a9ff9.appspot.com",
-    messagingSenderId: "604422688848",
-    appId: "1:604422688848:web:fb131e46b3e1b07a920155"
-  }
+  production: false
 };
 
 /*
